624fddf31967974d91ed8e3cf8757751ded573fd
Erictuan Nong <erictuan23@gmail.com>
2024-11-22 02:48:00 +0000

    Lecture Done

--- main.c
+++ main.c
@@ -32,12 +32,19 @@ void* my_malloc(size_t size) {
             // GOAL: split up the block into the malloc'd part and the free part
             // Round up size to next multiple of 8
             size_t rounded = ((size + 7) / 8) * 8;
-            *current = rounded + 1; // rounds up and sets the "busy" bit
-
-            // what if remaining is close to 0/8/16, etc.
-            size_t remaining = cur_size - (rounded + VAL_SIZE);
-            uint64_t* remaining_ptr = current + (rounded / VAL_SIZE) + 1;
-            *remaining_ptr = remaining;
+            
+            // what if remaining is close to 0/8/16, etc
+            if (cur_size >= rounded + 16) {
+                size_t remaining = cur_size - (rounded + VAL_SIZE);
+                uint64_t* remaining_ptr = current + (rounded / VAL_SIZE) + 1;
+                *remaining_ptr = remaining;
+                *current = rounded + 1; // rounds up and sets the "busy" bit
+                                        // when splitting a block, split by
+                                        // the amount ht a user requested
+            }
+            else {
+                *current += 1; // don't change size, just tag as busy!
+            }
             return current + 1;
         }
         else {
@@ -48,15 +55,62 @@ void* my_malloc(size_t size) {
     return NULL;
 }
 
+void print_heap() {
+    uint64_t* current = HEAP_START;
+    while (current < (HEAP_START + (HEAP_SIZE / VAL_SIZE))) {
+        uint64_t cur_header = *current;
+        uint64_t cur_size = (cur_header / 2) * 2;
+        printf("%p\t%d\t%d\n", current, cur_header % 2, cur_size);
+        uint64_t* next = current + (cur_size / VAL_SIZE) + 1;
+        current = next;
+    }
+    printf("\n\n");
+}
+
+void my_free(void* p) {
+    uint64_t* current = p;
+    uint64_t* header = current - 1;
+    if (*header % 2 == 1) { // else case: valgrind reporting double free!
+        *header = *header - 1;
+    }
+}
+
 int main() {
     init_heap();
-    //printf("%lu %lu\n", HEAP_START[0], HEAP_START[1]);
     int* a = my_malloc(40);
-    printf("%llu %llu %llu\n", HEAP_START[0], HEAP_START[1], HEAP_START[6]);
+    uint64_t* b = my_malloc(10);
+    b[1] = 3214819247891274;
 
-    int* b = my_malloc(10);
-    printf("%llu %llu\n", HEAP_START[6], HEAP_START[9]);
-
-    int* c = my_malloc(321);
-    printf("%p\n", c);
+    int* c = my_malloc(20);
+    my_free(b);
+    print_heap();
+    int* d = my_malloc(30); // this got allocated at the end of the heap
+    print_heap();
+    int* e = my_malloc(8); // what should we expect? (change 12 to 8)
+    print_heap();
 }
+
+/*                       (-) *current = rounded + 1
+________________________________________________________________________________________
+| 41 |   |   |   |   |   | 16 -> 17 |  |   | 25 |   |   |   | 33 |   |   |   |    | 148 |  ...
+________________________________________________________________________________________
+my_malloc(12)
+current = (-)
+cur_header = 16 0b10000 0x10
+rounded = 16
+remaining = 16 - (16 + 8) = -8
+
+why should we do +16 in the if statement?
+header for busy block     how much extra space (remaining)
+______________________________________________
+| cur_size |   |   |   |   |  new_free | ... |
+______________________________________________
+            <---size--->    
+which will take up some amount of space
+
+            (-) *current = rounded + 1   b[0] b[1]
+________________________________________________________________________________________
+| 41 |   |   |   |   |   | 16 -> 17 -> 9 |   |   | 25 |   |   |   | 33 |   |   |   |    | 248 |  ...
+________________________________________________________________________________________
+
+*/
